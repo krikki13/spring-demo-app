9dd4007d449200273651385bbc737c1b9a15141f
Kristjan Turnšek <kristjanturnsek@gmail.com>
2021-10-26 14:21:10 +0200

    Updated docker and docker compose files

---
+++ sql_init/mysql/init_mysql_db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE `demo`;
+CREATE TABLE demo.`dates` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
